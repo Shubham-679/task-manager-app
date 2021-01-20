d2bc05fd4553be01dc5741421ecbaa8eaa49dc69
Shubham-679 <kushwahshubham679.com>
2021-01-20 16:51:18 +0530

    heroku

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, ' client', 'build', 'index.html'));
 });
 
 mongoose.connect('mongodb://localhost/Dummy',{
