cb880d2ad4dcde3d1382cefaf6574998b5272042
Shubham-679 <kushwahshubham679.com>
2021-01-22 12:56:26 +0530

    1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/public', express.static('public'));
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
