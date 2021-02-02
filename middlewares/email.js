2d5b751442379c7ec09a734f6fa8931b8118f7fa
Shubham-679 <kushwahshubham679.com>
2021-02-02 15:11:53 +0530

    set api key

--- middlewares/email.js
+++ middlewares/email.js
@@ -1,55 +1,25 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
-console.log("email => ",process.env.adminEmail)
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.adminEmail,
-      pass: process.env.adminPassword
-    },
-    tls:{
-      rejectUnauthorized:false
-    }
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendNewTaskEmail = (email , name)=>{
 
-    let message = {
-        from: '"Task-Manager-App"kushwahshubham679@gmail.com',
-        to: email,
+    sgMail.send({
+        to : email,
+        from : 'kushwahshubham679@gmail.com',
         subject: 'New Task ✔',
         text: `Hello.,  ${name} You Have New Task In a Row.`,
-    };
-    
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            console.log('Error occurred. ' + err.message);
-            return process.exit(1);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        res.render('contact', {msg:'Email has been sent'});
-    });
+    })
 }
 
 const sendUpdateTaskEmail = (email, name)=>{
     
-    let message = {
-        from: '"Task-Manager-App"kushwahshubham679@gmail.com',
-        to: email,
+    sgMail.send({
+        to : email,
+        from : 'kushwahshubham679@gmail.com',
         subject: 'Task Updated ✔',
         text: `Hello.,  ${name} You Have New Updations In Your Tasks.`,
-    };
-  
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            console.log('Error occurred. ' + err.message);
-            return process.exit(1);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        res.render('contact', {msg:'Email has been sent'});
-    });
+    })
 }
 
 module.exports = {
